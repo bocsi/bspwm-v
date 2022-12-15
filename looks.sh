17bd46df02cc2f9ff18af1784bed5a5d50a71fc6
bocsi <60975283+bocsi@users.noreply.github.com>
2022-12-14 18:31:10 -0800

    Update looks.sh

--- looks.sh
+++ looks.sh
@@ -6,3 +6,10 @@ cd Nordzy-icon/
 ./install.sh -t turquoise -c dark
 cd ..
 rm -rf Nordzy-icon
+
+# gtk theme
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+cd WhiteSur-gtk-theme/
+./install.sh -l -c Dark -a alt -t yellow -i void --nord
+cd ..
+rm -rf WhiteSur-gtk-theme/
